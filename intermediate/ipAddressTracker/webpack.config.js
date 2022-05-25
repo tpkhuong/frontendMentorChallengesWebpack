11b8794365bf373dde3df14ee4a9a70655b5d4f8
Toan <tpkhuong@gmail.com>
2022-05-25 04:09:04 -0400

    changed webpack.config.js

--- intermediate/ipAddressTracker/webpack.config.js
+++ intermediate/ipAddressTracker/webpack.config.js
@@ -1,6 +1,7 @@
 const path = require("path");
 const HtmlWebpackPlugin = require("html-webpack-plugin");
 const Dotenv = require("dotenv-webpack");
+const webpack = require("webpack");
 
 module.exports = {
   entry: "./src/index.js",
