5a43e9babaa765a6947f09be6bf81045766dbd25
Toan <tpkhuong@gmail.com>
2022-10-11 04:06:18 -0400

    enjoyed the effort/process of building algorithm scroll snap feature for home page of portfolio.

--- intermediate/urlShorteningApiPage/frontend/src/Components/App/App.js
+++ intermediate/urlShorteningApiPage/frontend/src/Components/App/App.js
@@ -1,12 +1,14 @@
 import React from "react";
 // import "../../styles.css";
 import TestStyles from "./App.module.css";
+import PortfolioStyles from "./PortfolioComponent.module.css";
 // import { bindToClick } from "./funcs";
 
 export default function App({ children, ...props }) {
   return (
     <React.Fragment>
-      <TestApp />
+      {/* <TestApp /> */}
+      <PortfolioComponent />
     </React.Fragment>
   );
 }
@@ -92,6 +94,99 @@ function TestApp({ children, ...props }) {
         </div>
       </button>
       <span className={TestStyles[`blinking`]}>_hello</span>
+    </React.Fragment>
+  );
+}
+
+function PortfolioComponent({ children, ...props }) {
+  const objOfMethods = {
+    ArrowDown: (event, element) => {
+      // next element tabIndex 0
+      // prev element tabIndex -1
+
+      if (!element.nextElementSibling) {
+        // first child tabindex 0
+        element.parentElement.firstElementChild.setAttribute("tabindex", "0");
+        element.parentElement.firstElementChild.focus();
+        // last child tabindex -1
+        element.parentElement.lastElementChild.setAttribute("tabindex", "-1");
+        return;
+      }
+      element.nextElementSibling.setAttribute("tabindex", "0");
+      element.nextElementSibling.focus();
+      const currentElement = document.activeElement;
+      // call scrollTo
+      // currentElement.getBoundingClientRect();
+      // sibling element of parent element(scroll container)
+      // top: 275, bottom: 625
+      console.log(currentElement.getBoundingClientRect());
+      // console.log(
+      //   currentElement.parentElement.previousElementSibling.getBoundingClientRect()
+      // );
+      const top =
+        currentElement.parentElement.previousElementSibling.getBoundingClientRect()
+          .top;
+      currentElement.scrollTo(0, top);
+      currentElement.previousElementSibling.setAttribute("tabindex", "-1");
+      // console.log(element.parentElement.firstElementChild);
+    },
+    ArrowUp: (event, element) => {
+      // prev element tabIndex 0
+      // next element tabIndex -1
+      // console.log(element.parentElement.lastElementChild);
+      if (!element.previousElementSibling) {
+        // last child tabindex 0
+        element.parentElement.lastElementChild.setAttribute("tabindex", "0");
+        element.parentElement.lastElementChild.focus();
+        // first child tabindex -1
+        element.parentElement.firstElementChild.setAttribute("tabindex", "-1");
+        return;
+      }
+      element.previousElementSibling.setAttribute("tabindex", "0");
+      element.previousElementSibling.focus();
+      const currentElement = document.activeElement;
+      // call scrollTo
+      // currentElement.getBoundingClientRect();
+      // sibling element of parent element(scroll container)
+      // top: 275, bottom: 625
+      // console.log(
+      //   currentElement.parentElement.previousElementSibling.getBoundingClientRect()
+      // );
+      /**
+       * using scrollTo on parent element with overflow-y: auto
+       * and scroll-snap-type works. scrollTo(0,275)
+       * **/
+      // document.elementFromPoint(600,249.60000610351562) focusing element working with
+      // scroll and arrow keys
+      const top =
+        currentElement.parentElement.previousElementSibling.getBoundingClientRect()
+          .top;
+      currentElement.scrollTo(0, top);
+      currentElement.nextElementSibling.setAttribute("tabindex", "-1");
+    },
+  };
+  return (
+    <React.Fragment>
+      <div className={PortfolioStyles[`wrapper`]}>
+        <div className={PortfolioStyles[`text-content`]}></div>
+        <div
+          id="scroll-container"
+          onKeyDown={(event) => {
+            event.preventDefault();
+            const currentElement = document.activeElement;
+            console.log(event);
+            objOfMethods[event.code](event, currentElement);
+            event.target.getBoundingClientRect();
+          }}
+          className={PortfolioStyles[`container`]}
+        >
+          <div tabIndex="0" className={PortfolioStyles[`snap-item`]}></div>
+          <div tabIndex="-1" className={PortfolioStyles[`snap-item`]}></div>
+          <div tabIndex="-1" className={PortfolioStyles[`snap-item`]}></div>
+          <div tabIndex="-1" className={PortfolioStyles[`snap-item`]}></div>
+          <div tabIndex="-1" className={PortfolioStyles[`snap-item`]}></div>
+        </div>
+      </div>
     </React.Fragment>
   );
 }
